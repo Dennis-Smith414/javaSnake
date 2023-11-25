cab9406e982c6a8a1879603f2e34c8c39766b98b
Dennis <dennisnsmith2000@gmail.com>
2023-11-25 10:10:28 -0600

    set up of project

---
+++ javaSnake/src/javaSnake/GamePanel.java
@@ -0,0 +1,57 @@
+package javaSnake;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import java.util.Random;
+public class GamePanel extends JPanel implements ActionListener{
+	
+	static final int SCREEN_WIDTH = 800;
+	static final int SCREEN_HEIGHT = 800;
+	static final int UNIT_SIZE = 25;
+	static final int GAME_UNITS = (SCREEN_WIDTH*SCREEN_HEIGHT)/UNIT_SIZE;
+	
+	GamePanel() {
+		
+	}
+	public void startGame() {
+		
+	}
+	
+	public void paintComponent(Graphics g) {
+		
+	}
+	
+	public void draw(Graphics2D g) {
+		
+	}
+	
+	public void move() {
+		
+	}
+	
+	public void checkApple() {
+		
+	}
+	
+	public void checkCollisions() {
+		
+	}
+	
+	public void gameOver(Graphics g) {
+		
+	}
+	
+	
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	public class MyKeyAdapter extends KeyAdapter {
+		@Override
+		public void keyPressed(KeyEvent e) {
+			
+		}
+	}
+}
